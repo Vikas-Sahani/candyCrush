9e048b2ae9bc8adde5c25394be87a110042062d2
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2022-10-14 10:38:26 +0530

    correceted setences of GameRules

--- src/components/GameRules.js
+++ src/components/GameRules.js
@@ -13,17 +13,17 @@ export default function GameRule(props) {
           <h3>Game Rules</h3>
 
           <li>
-            Drag and Drop only left side, top side, right side or bottom side,
-            and only one step you can drag and drop
+            Drag and Drop only left side, top side, right side and bottom side.
+            And you can only drag and Drop the candys for one step.
           </li>
           <li>
-            If 3 rows or columns are matching then score will increase by 3
+            If 3 rows or columns are matched then score will increase by 3.
           </li>
           <li>
-            If 4 rows or columns are matching then score will increase by 4
+            If 4 rows or columns are matched then score will increase by 4
           </li>
           <li>
-            If 5 rows or columns are matching then score will increase by 5
+            If 5 rows or columns are matched then score will increase by 5
           </li>
         </ul>
         <button
